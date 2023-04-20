6559b49535b1f313daee52aa389fce054951b5fa
root <root@maymine>
2023-04-20 09:39:47 -0500

    añadiendo tailwindcss v1

--- app/controllers/authentication/users_controller.rb
+++ app/controllers/authentication/users_controller.rb
@@ -1,4 +1,5 @@
 class Authentication::UsersController < ApplicationController
+    skip_before_action :protect_pages
     def new
         @user = User.new
     end
@@ -7,6 +8,7 @@ class Authentication::UsersController < ApplicationController
         @user = User.new(user_params)
         
         if @user.save
+            session[:user_id] =  @user.id
             redirect_to banks_path, notice: t('.created')
         else
             render :new, status: :unprocessable_entity
@@ -16,7 +18,6 @@ class Authentication::UsersController < ApplicationController
     private 
 
     def user_params
-        params.require(:user),permit(:email, :username, :password)
+        params.require(:user).permit(:email, :username, :password)
     end
-
 end
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,4 +6,6 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 
+import  { alert } from "tailwindcss-stimulus-components"
+application.register('alert', Alert)
 export { application }
