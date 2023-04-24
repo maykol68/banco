b10f1c81739bf9de1f0a8136f485f7ec85f815c1
root <root@maymine>
2023-04-24 15:21:04 -0500

    reparando bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: { case_sensitive: false }, 
             length: { in: 3..15},
             format: {
-                with: /\A[a-z-0-9-A-Z]+\z/,
+                with: /\A[a-z0-9A-Z]+\z/,
                 message: :invalid
             }
     validates :password, length: {minimum: 7}
